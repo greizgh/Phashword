fbd4643364f9d38416a212cbd1a6936aa78f6726
Greizgh <greizgh@ephax.org>
2016-06-23 07:37:46 +0200

    Add popup style

--- src/popup.jsx
+++ src/popup.jsx
@@ -3,10 +3,14 @@ import {render} from 'react-dom';
 import KeyGenerator from './popup/keygenerator.jsx';
 import SiteProfile from './popup/siteprofile.jsx';
 
+const popupStyle = {
+    width: '300px',
+};
+
 class Popup extends React.Component {
   render() {
     return (
-      <div className="popup">
+      <div className="popup" style={popupStyle}>
         <SiteProfile/>
         <KeyGenerator/>
         <button>Settings</button>
