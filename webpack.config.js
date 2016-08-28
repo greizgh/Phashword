c012213d50edb4137467d2d65167fe13b90bbd4d
Greizgh <greizgh@ephax.org>
2016-08-28 19:27:24 +0200

    Define NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
@@ -23,4 +24,9 @@ module.exports = {
       ],
     }],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
+    })
+  ],
 };
