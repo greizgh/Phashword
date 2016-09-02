5d2b91f59104c0c06e41b9aaddaee5ef06ecdac9
Greizgh <greizgh@ephax.org>
2016-09-02 07:26:07 +0200

    Add popup action

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Popup from './components/popup.jsx';
+import { openSettings } from './actions';
 
 function dispatch(action) {
   chrome.runtime.sendMessage(action);
@@ -10,8 +11,12 @@ function onClose() {
   window.close();
 }
 
+function onSettings() {
+  chrome.runtime.sendMessage(openSettings());
+}
+
 const popup = render(
-  <Popup dispatch={dispatch} onClose={onClose} />,
+  <Popup dispatch={dispatch} onClose={onClose} onSettings={onSettings}/>,
   document.getElementById('quick-settings')
 );
 
