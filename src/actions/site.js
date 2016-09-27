9e0850191373750104fad2c66f8caeb53484e243
Greizgh <greizgh@ephax.org>
2016-09-27 21:50:04 +0200

    reference site settings by id

--- src/actions/site.js
+++ src/actions/site.js
@@ -7,30 +7,30 @@ export const SET_TAG = 'SET_TAG';
 export const SET_LENGTH = 'SET_LENGTH';
 export const SET_TYPE = 'SET_TYPE';
 
-export function addSite(hostname) {
-  return { type: ADD_SITE, hostname: hostname };
+export function addSite(id) {
+  return { type: ADD_SITE, id: id };
 }
 
-export function deleteSite(hostname) {
-  return { type: DELETE_SITE, hostname: hostname };
+export function deleteSite(id) {
+  return { type: DELETE_SITE, id: id };
 }
 
-export function toggleSite(hostname) {
-  return { type: TOGGLE_SITE, hostname: hostname };
+export function toggleSite(id) {
+  return { type: TOGGLE_SITE, id: id };
 }
 
-export function setTag(hostname, tag) {
-  return { type: SET_TAG, hostname: hostname, tag: tag };
+export function setTag(id, tag) {
+  return { type: SET_TAG, id: id, tag: tag };
 }
 
-export function setProfile(hostname, profileId) {
-  return { type: SET_PROFILE, hostname: hostname, profileId: profileId };
+export function setProfile(id, profileId) {
+  return { type: SET_PROFILE, id: id, profileId: profileId };
 }
 
-export function setLength(hostname, length) {
-  return { type: SET_LENGTH, hostname: hostname, length: length };
+export function setLength(id, length) {
+  return { type: SET_LENGTH, id: id, length: length };
 }
 
-export function setType(hostname, passwordType) {
-  return { type: SET_TYPE, hostname, passwordType: passwordType };
+export function setType(id, passwordType) {
+  return { type: SET_TYPE, id, passwordType: passwordType };
 }
--- src/reducers/site.js
+++ src/reducers/site.js
@@ -13,27 +13,27 @@ export default function sitesReducer(state = Map(), action) {
   switch (action.type) {
     case ADD_SITE:
       // Todo populate site settings
-      return state.set(action.hostname, {});
+      return state.set(action.id, {});
     case TOGGLE_SITE:
-      return state.update(action.hostname, (site) => {
+      return state.update(action.id, (site) => {
         return { ...site, enabled: !site.enabled };
       });
     case DELETE_SITE:
-      return state.delete(action.hostname);
+      return state.delete(action.id);
     case SET_PROFILE:
-      return state.update(action.hostname, (site) => {
+      return state.update(action.id, (site) => {
         return { ...site, profileId: action.profileId };
       });
     case SET_LENGTH:
-      return state.update(action.hostname, (site) => {
+      return state.update(action.id, (site) => {
         return { ...site, length: action.length };
       });
     case SET_TAG:
-      return state.update(action.hostname, (site) => {
+      return state.update(action.id, (site) => {
         return { ...site, tag: action.tag };
       });
     case SET_TYPE:
-      return state.update(action.hostname, (site) => {
+      return state.update(action.id, (site) => {
         return { ...site, type: action.passwordType };
       });
     default:
