ba37a1fdd2ab0fe1d130f4481b5416d00cf6647a
Greizgh <greizgh@ephax.org>
2016-09-28 08:08:57 +0200

    Add stories for quickstate

--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Popup from '../src/components/popup.jsx';
+import QuickState from '../src/components/popup/quicksettings.jsx';
 import '../css/popup.css';
 
-storiesOf('Popup', module)
+storiesOf('popup', module)
   .add('Empty state', () => (
     <Popup
       onSettings={action('settings')}
@@ -17,3 +18,33 @@ storiesOf('Popup', module)
       onLengthChange={action('change type')}
     />
   ));
+
+const profile = {
+  id: '1234567898',
+  name: 'Default',
+  default: true,
+  color: '#abcdff',
+  length: 12,
+  type: 3,
+  privateKey: 'private',
+};
+
+storiesOf('popup.quickstate', module)
+  .add('Site disabled', () =>
+    <QuickState
+      currentProfile={0}
+      profiles={[profile]}
+      enabled={false}
+      onToggle={action('toggle state')}
+      onProfileChange={action('profile change')}
+    />
+  )
+  .add('Site enabled', () =>
+    <QuickState
+      currentProfile={0}
+      profiles={[profile]}
+      enabled={true}
+      onToggle={action('toggle state')}
+      onProfileChange={action('profile change')}
+    />
+  );
