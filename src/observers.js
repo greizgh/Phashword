242ed652225a8bf13f6f0c9c55e057e49197a4fe
Greizgh <greizgh@ephax.org>
2016-10-02 16:08:48 +0200

    Prefer use of isEmpty rather than count

--- src/observers.js
+++ src/observers.js
@@ -8,7 +8,7 @@ store.subscribe((state) => { lastKnownState = state; });
 
 // Make sure there is a profile and if there is only one make it default
 export function defaultProfileGenerator(state) {
-  if (state.profiles.size === 0) {
+  if (state.profiles.isEmpty()) {
     dispatcher.onNext(createProfile());
   }
   if (state.profiles.size === 1 && state.profiles.count((profile) => profile.default) === 0) {
@@ -19,7 +19,7 @@ export function defaultProfileGenerator(state) {
 // Make sure there is a default profile.
 // Useful after profile deletion
 export function defaultProfileSelector(state) {
-  if (state.profiles.size > 0 && state.profiles.count((profile) => profile.default) === 0) {
+  if (!state.profiles.isEmpty() && state.profiles.count((profile) => profile.default) === 0) {
     dispatcher.onNext(setDefaultProfile(state.profiles.slice(0).keySeq().first()));
   }
 }
