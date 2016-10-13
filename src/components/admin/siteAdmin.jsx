759cf2744d531a3f2a41360777d70d0a3e750110
Greizgh <greizgh@ephax.org>
2016-10-13 18:40:01 +0200

    Add filter to site list

--- src/components/admin/siteAdmin.jsx
+++ src/components/admin/siteAdmin.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import SiteItem from './siteItem.jsx';
 
 export default class SiteAdmin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sites: props.sites,
+      filter: '',
+    }
+    this.setFilter = this.setFilter.bind(this);
+  }
+  setFilter(event) {
+    this.setState({...this.state, filter: event.target.value});
+  }
   render() {
     return (
       <div>
+        <input type="text" value={this.state.filter} onChange={this.setFilter} />
         <div>
-          {this.props.sites.map((site) => (
+          {this.props.sites
+            .filter((site) => site.id.startsWith(this.state.filter))
+            .map((site) => (
             <SiteItem site={site} onDelete={this.props.onDelete} />
           ))}
         </div>
