f6b4f3d0a68a57a74447fb40d7fb2b2d5906725d
Greizgh <greizgh@ephax.org>
2016-10-25 08:16:04 +0200

    Improve profile admin style

--- src/components/admin/profileItem.jsx
+++ src/components/admin/profileItem.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Radium from 'radium';
-import { labelStyle, inputStyle, textInputStyle, selectStyle } from '../style.js';
+import {
+  labelStyle,
+  inputStyle,
+  textInputStyle,
+  selectStyle,
+  panelFormItemStyle,
+} from '../style.js';
 
 class ProfileItem extends React.Component {
   constructor(props) {
@@ -13,54 +19,66 @@ class ProfileItem extends React.Component {
   render() {
     return (
       <div>
-        <label htmlFor="name" style={labelStyle}>Name</label>
-        <input
-          style={[inputStyle, textInputStyle]}
-          type="text"
-          id="name"
-          key={'name'}
-          value={this.props.profile.name}
-          onChange={this.onNameChange}
-        />
-        <label htmlFor="color" style={labelStyle}>Color</label>
-        <input
-          type="color"
-          id="color"
-          value={this.props.profile.color}
-          onChange={this.onColorChange}
-        />
-        <label htmlFor="length" style={labelStyle}>Length</label>
-        <input
-          type="number"
-          style={[inputStyle, textInputStyle]}
-          id="length"
-          key={'length'}
-          min="1"
-          value={this.props.profile.length}
-          onChange={this.onLengthChange}
-        />
-        <label style={labelStyle} htmlFor="type">Type</label>
-        <select
-          style={selectStyle}
-          key={'type'}
-          id="type"
-          value={this.props.profile.type}
-          onChange={this.onTypeChange}
-        >
-          <option key={1} value="1">Alphanumeric and special characters</option>
-          <option key={2} value="2">Alphanumeric</option>
-          <option key={3} value="3">Numeric</option>
-        </select>
-        <label style={labelStyle} htmlFor="private_key">Private key</label>
-        <input
-          type="text"
-          style={[inputStyle, textInputStyle]}
-          key={'private'}
-          id="private_key"
-          value={this.props.profile.privateKey}
-          onChange={this.onPrivateKeyChange}
-        />
-        <button key="delbtn" onClick={this.deleteProfile}>Delete</button>
+        <div style={panelFormItemStyle}>
+          <label htmlFor="name" style={labelStyle}>Name</label>
+          <input
+            style={[inputStyle, textInputStyle]}
+            type="text"
+            id="name"
+            key={'name'}
+            value={this.props.profile.name}
+            onChange={this.onNameChange}
+          />
+        </div>
+        <div style={panelFormItemStyle}>
+          <label htmlFor="color" style={labelStyle}>Color</label>
+          <input
+            type="color"
+            id="color"
+            value={this.props.profile.color}
+            onChange={this.onColorChange}
+          />
+        </div>
+        <div style={panelFormItemStyle}>
+          <label htmlFor="length" style={labelStyle}>Length</label>
+          <input
+            type="number"
+            style={[inputStyle, textInputStyle]}
+            id="length"
+            key={'length'}
+            min="1"
+            value={this.props.profile.length}
+            onChange={this.onLengthChange}
+          />
+        </div>
+        <div style={panelFormItemStyle}>
+          <label style={labelStyle} htmlFor="type">Type</label>
+          <select
+            style={selectStyle}
+            key={'type'}
+            id="type"
+            value={this.props.profile.type}
+            onChange={this.onTypeChange}
+          >
+            <option key={1} value="1">Alphanumeric and special characters</option>
+            <option key={2} value="2">Alphanumeric</option>
+            <option key={3} value="3">Numeric</option>
+          </select>
+        </div>
+        <div style={panelFormItemStyle}>
+          <label style={labelStyle} htmlFor="private_key">Private key</label>
+          <input
+            type="text"
+            style={[inputStyle, textInputStyle]}
+            key={'private'}
+            id="private_key"
+            value={this.props.profile.privateKey}
+            onChange={this.onPrivateKeyChange}
+          />
+        </div>
+        <div style={panelFormItemStyle}>
+          <button key="delbtn" onClick={this.deleteProfile}>Delete</button>
+        </div>
       </div>
     );
   }
