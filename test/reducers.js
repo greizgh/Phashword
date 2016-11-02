d08b953a717a78005fc5cb06be2c75c0b3f4441e
Greizgh <greizgh@ephax.org>
2016-11-02 07:44:54 +0100

    Add missing test for appReducer

--- test/reducers.js
+++ test/reducers.js
@@ -1,10 +1,14 @@
 import { assert } from 'chai';
-import { setCurrentProfile } from '../src/actions.js';
+import { setCurrentProfile, setCurrentSite } from '../src/actions.js';
 import appReducer from '../src/reducers.js';
 
 describe('appReducer', () => {
   it('should handle profile change', () => {
     const state = appReducer({}, setCurrentProfile(2));
     assert.equal(state.currentProfile, 2);
+  });
+  it('should handle site change', () => {
+    const state = appReducer({}, setCurrentSite('test'));
+    assert.equal(state.currentSite, 'test');
   });
 });
